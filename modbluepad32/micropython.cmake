19353473cdc8c7dcc942da991b1deff11e6f32db
Ahmad Cooper <108217835+AhmadAC@users.noreply.github.com>
2026-06-08 09:51:48 +0800

    Update micropython.cmake

--- modbluepad32/micropython.cmake
+++ modbluepad32/micropython.cmake
@@ -1,7 +1,7 @@
 # Create the user C module library
 add_library(usermod_bluepad32 INTERFACE)
 
-# Find all C files in this folder automatically (no need to specify exact filenames)
+# Find all C files in this folder automatically
 file(GLOB USERMOD_SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.c)
 target_sources(usermod_bluepad32 INTERFACE ${USERMOD_SOURCES})
 
@@ -10,7 +10,3 @@ target_include_directories(usermod_bluepad32 INTERFACE ${CMAKE_CURRENT_LIST_DIR}
 
 # Link the user module to MicroPython
 target_link_libraries(usermod INTERFACE usermod_bluepad32)
-
-# VERY IMPORTANT: Link the Bluepad32 and BTStack ESP-IDF components
-# This allows your C code to successfully #include "uni.h" and "btstack_run_loop.h"
-target_link_libraries(usermod_bluepad32 INTERFACE idf::bluepad32 idf::btstack)
