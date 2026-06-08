5dd7715405ddbee5fdfd92921067b0f3c8ec0b7a
AhmadAC <cooperahmad@gmail.com>
2026-06-08 12:35:16 +0800

    ..

--- modbluepad32/micropython.cmake
+++ modbluepad32/micropython.cmake
@@ -11,6 +11,7 @@ target_include_directories(usermod_bluepad32 INTERFACE
     $ENV{GITHUB_WORKSPACE}/bluepad32/src/components/bluepad32
     $ENV{GITHUB_WORKSPACE}/bluepad32/src/components/bluepad32/include
     $ENV{GITHUB_WORKSPACE}/bluepad32/external/btstack/src
+    $ENV{GITHUB_WORKSPACE}/bluepad32/external/btstack/port/esp32/components/btstack/include
 )
 
 # Link the user module to MicroPython
