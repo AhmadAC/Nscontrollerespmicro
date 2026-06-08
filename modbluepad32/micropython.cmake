b4a1715159624797a397604742e5b62495ac2829
Ahmad Cooper <108217835+AhmadAC@users.noreply.github.com>
2026-06-08 10:05:49 +0800

    Update micropython.cmake

--- modbluepad32/micropython.cmake
+++ modbluepad32/micropython.cmake
@@ -5,8 +5,13 @@ add_library(usermod_bluepad32 INTERFACE)
 file(GLOB USERMOD_SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.c)
 target_sources(usermod_bluepad32 INTERFACE ${USERMOD_SOURCES})
 
-# Include the current directory
-target_include_directories(usermod_bluepad32 INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+# Explicitly include the local directory AND the Bluepad32 header directories
+# so the compiler can find "uni.h" and the BTStack dependencies
+target_include_directories(usermod_bluepad32 INTERFACE 
+    ${CMAKE_CURRENT_LIST_DIR}
+    $ENV{EXTRA_COMPONENT_DIRS}/bluepad32/include
+    $ENV{EXTRA_COMPONENT_DIRS}/btstack/include
+)
 
 # Link the user module to MicroPython
 target_link_libraries(usermod INTERFACE usermod_bluepad32)
